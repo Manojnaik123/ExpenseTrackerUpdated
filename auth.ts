75a7d5357e4f345016d57d3e09bae1b3e67afa2e
Manojnaik123 <manojnaikbse@gmail.com>
2026-02-09 13:52:36 +0530

    hi

--- auth.ts
+++ auth.ts
@@ -8,6 +8,10 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pa2plZmRueW1mZ2hzYnR6bnViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNzg3NzksImV4cCI6MjA4NDc1NDc3OX0.AH-V3gFKSX564PGltXn3IE2ieZ6RU___oK5xCtGVkgI"
 )
 
+console.log('`process.env.GOOGLE_CLIENT_ID`');
+console.log(process.env.GOOGLE_CLIENT_ID);
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
