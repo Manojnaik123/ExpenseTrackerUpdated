524dcb5e168b10df56e90e8cd4a352d44e3fef1f
Manojnaik123 <manojnaikbse@gmail.com>
2026-02-17 07:51:16 +0530

    polished

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,3 @@ import { handlers } from "@/auth";
 export const { GET, POST } = handlers
 
 
-/**
- * 
- * Feb 9, 11:58:16 AM: 810d67d9 ERROR  [auth][error] UntrustedHost: Host must be trusted. URL was: https://expensetrackerrrrrrrrrr.netlify.app/api/auth/session. Read more at https://errors.authjs.dev#untrustedhostFeb 9, 11:58:16 AM: 810d67d9 ERROR      at /var/task/.next/server/chunks/ssr/_cf35bd72._.js:418:51260
-    at fR (/var/task/.next/server/chunks/ssr/_cf35bd72._.js:418:53944)
- */
